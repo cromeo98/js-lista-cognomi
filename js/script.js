dd422f3150df2de22c3a00fac078145aeefc77e0
cromeo98 <romeo.christian98@gmail.com>
2021-05-31 17:12:45 +0200

    add: array dtb email registered

--- js/script.js
+++ js/script.js
@@ -5,7 +5,11 @@
 
 // chiedo email all'utente
 var userEmail = prompt('Inserisci la tua e-mail')
-console.log(userEmail);
+// console.log(userEmail);
+
+// creo array database e-mail registrate (Si può provare a creare un random generator di email con un ciclo for? --> da provare)
+var registeredUsers = [ '0hyunw@freeallapp.com' , 'ilio.east5@helpman.ml' , 'qaziz@usbvap.com' , 'oichimarugin123y@jeruknipis.buzz' , '2amira@usayoman.com' , 'amohame@tapiitudulu.com' ]
+console.log(registeredUsers);
 
 // Lista Cognomi:
 // chiedi all’utente il cognome
