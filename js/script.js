71e803f0f9cf4b5a3e892030d21f5bdca05bfc87
cromeo98 <romeo.christian98@gmail.com>
2021-05-31 17:03:04 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+// Lista Cognomi:
+// chiedi all’utente il cognome
+// inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
